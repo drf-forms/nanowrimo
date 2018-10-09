6eb7ac489d3008704db3f69572f94ab8dc60e10b
Emmanuelle Delescolle <emma@levit.be>
2018-10-09 18:22:57 +0200

    :heavy_plus_sign: basic models and endpoints

--- back/nanowrimo/endpoints.py
+++ back/nanowrimo/endpoints.py
@@ -1,9 +1,19 @@
 from django.contrib.contenttypes.models import ContentType
 
-from drf_auto_endpoint.router import router
+from drf_auto_endpoint.endpoints import Endpoint
+from drf_auto_endpoint.router import router, register
 
 from levit_report.models import Document
+from .models import Book, Chapter, Place, Prop, Character, Scene, InventoryExchange
 
 
-router.register(Document)
+@register
+class BookEndpoint(Endpoint):
+    model = Book
+    exclude_fields = ('author', )
+
+
+for Model in (Document, Chapter, Place, Prop, Character, Scene, InventoryExchange):
+    router.register(Model)
+
 router.register(ContentType, read_only=True, list_me=False)
---
+++ back/nanowrimo/models.py
@@ -0,0 +1,98 @@
+from django.conf import settings
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=500, null=True, blank=True)
+    summary = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                               related_name='authors')
+
+    def __str__(self):
+        if title is not None and title != '':
+            return self.title
+        return 'Untitled book #{}'.format(self.id)
+
+
+class Chapter(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='chapters')
+    number = models.PositiveSmallIntegerField(default=0)
+    title = models.CharField(max_length=500, null=True, blank=True)
+    summary = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        if title is not None and title != '':
+            return self.title
+        return 'Chapter {}'.format(self.number)
+
+
+class Place(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='places')
+    name = models.CharField(max_length=500)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Prop(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='props')
+    name = models.CharField(max_length=500)
+    descritpion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='characters')
+    name = models.CharField(max_length=500)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Scene(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='scenes')
+    ordernum = models.PositiveSmallIntegerField(default=0)
+    timestamp = models.DateTimeField(null=True)
+    short_description = models.CharField(max_length=500, null=True, blank=True)
+    summary = models.TextField(null=True, blank=True)
+    long_description = models.TextField(null=True, blank=True)
+    chapter = models.ForeignKey(Chapter, on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name='scenes')
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True,
+                              related_name='scenes')
+    characters = models.ManyToManyField(Character, related_name='scenes')
+
+    def __str__(self):
+        if self.short_description is not None and self.short_description != '':
+            return '{} ({})'.format(self.short_description, self.timestamp)
+        return 'scene at {} with {} ({})'.format(
+            self.place.name,
+            ', '.join([c.name for c in self.characters.all()]),
+            self.timestamp
+        )
+
+
+class InventoryExchange(models.Model):
+    scene = models.ForeignKey(Scene, on_delete=models.CASCADE, related_name='exchanged')
+    from_char = models.ForeignKey(Character, on_delete=models.SET_NULL, null=True, blank=True,
+                                  related_name='given_props')
+    to_char = models.ForeignKey(Character, on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name='received_props')
+    prop = models.ForeignKey(Prop, on_delete=models.CASCADE, related_name='exchanged')
+    note = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        if self.note is not None and self.note != '':
+            return self.note
+        elif self.from_char is not None:
+            if self.to_char is None:
+                return '{} loses {}'.format(self.from_char.name, self.prop.name)
+            return '{} receives {} from {}'.format(self.to_char.name, self.prop.name,
+                                                   self.from_char.name)
+        elif self.to_char is not None:
+            return '{} receives {}'.format(self.to_char.name, self.prop.name)
+        return self.prop.name
